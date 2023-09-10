d05125ab8b853d8e6f5992937364d92dd80bde8b
MohamedGado <boogado@yahoo.com>
2023-09-10 10:22:47 +0300

    * fix(constants.ts): decrease SEARCH_DEBOUNCE value from 500 to 400

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,7 +1,7 @@
 const TOP_OFFSET = 60
 const STREAMING_MOVIES_API_URL = 'https://vidsrc.to/embed'
 const SEARCH_ACTOR_GOOGLE = 'https://www.google.com/search?q='
-const SEARCH_DEBOUNCE = 500
+const SEARCH_DEBOUNCE = 400
 
 export {
   TOP_OFFSET,
