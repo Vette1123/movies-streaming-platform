0774f99f76f80c1b18d2dcc4bd221f1876b96101
MohamedGado <boogado@yahoo.com>
2023-09-15 17:44:56 +0300

    revert(next.config.js): enable image optimization back, to monitor the difference

--- components/carousel.tsx
+++ components/carousel.tsx
@@ -2,17 +2,21 @@
 
 import React from 'react'
 import SwipeableViews from 'react-swipeable-views'
+import { autoPlay } from 'react-swipeable-views-utils'
+
+const AutoPlaySwipeableViews = autoPlay(SwipeableViews)
 
 export const Carousel = ({ children }: { children: React.ReactNode }) => {
   return (
-    <SwipeableViews
+    <AutoPlaySwipeableViews
       enableMouseEvents
       className="cursor-grabbing"
       loop
       animateTransitions={true}
       threshold={1}
+      autoPlay={true}
     >
       {children}
-    </SwipeableViews>
+    </AutoPlaySwipeableViews>
   )
 }
