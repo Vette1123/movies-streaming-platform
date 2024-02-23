950ae3a77729a612ca62f7ea1c79da1f79a95331
Mohamed Gado <boogado@yahoo.com>
2024-02-23 16:07:05 +0200

    Update streaming movies API URL

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,6 +1,6 @@
 const TOP_OFFSET = 60
-// const STREAMING_MOVIES_API_URL = 'https://vidsrc.to/embed'
-const STREAMING_MOVIES_API_URL = 'https://vidsrc.pro/embed'
+const STREAMING_MOVIES_API_URL = 'https://vidsrc.to/embed'
+// const STREAMING_MOVIES_API_URL = 'https://vidsrc.pro/embed'
 const SEARCH_ACTOR_GOOGLE = 'https://www.google.com/search?q='
 const SEARCH_DEBOUNCE = 400
 
